6febd84d05b6b9d66102ec8cec7f136bb9948b87
Dmytro Verovkin <dmitry335@gmail.com>
2023-06-07 20:07:42 +0300

    index.py

---
+++ api/index.py
@@ -0,0 +1,8 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(self.headers.get('x-forwarder-for').enccode())
+        return
